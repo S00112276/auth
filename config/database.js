d34c579c8c090971f6cc4fc0ef2f8313dd5c6663
Daniel O'Regan <S00112276@mail.itsligo.ie>
2017-11-07 14:04:20 +0000

    auth

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://joesbikes:LeeSin-2.@joesbikes-shard-00-00-teyoz.mongodb.net:27017,joesbikes-shard-00-01-teyoz.mongodb.net:27017,joesbikes-shard-00-02-teyoz.mongodb.net:27017/test?ssl=true&replicaSet=joesbikes-shard-0&authSource=admin',
+    secret: 'yoursecret'
+}
